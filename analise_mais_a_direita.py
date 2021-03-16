309570a7086715a725a9306ec779de4e7a032b86
Marcone de Freitas Marques <72458067+Marcone-sudo@users.noreply.github.com>
2021-03-16 08:53:59 -0300

    Update analise_mais_a_direita.py

--- analise_mais_a_direita.py
+++ analise_mais_a_direita.py
@@ -1,4 +1,5 @@
 #coding: utf-8
+import os
 dicionario_de_as = {}
 '''
 Função que analisa o arquivo e realiza os seguintes passos:
@@ -6,6 +7,8 @@ Função que analisa o arquivo e realiza os seguintes passos:
     ou seja, no exemplo "28329 6453 38040 23969" o valor da chave é "23969".
     E para seu valor são adicionados todos os prefixos sem repetição correspondentes.
 '''
+path = os.path.abspath(__file__)
+path = os.path.dirname(path)
 
 def processaLote(lote):
     """
@@ -15,14 +18,16 @@ def processaLote(lote):
     #Pego os valores de interesse
     aux_prefixo, aux_as = lote.split("|")[1:3]
     #Realizo as verificações necessárias a fim de adicionar os valores no dicionário.
-    if aux_as in dicionario_de_as.keys():
-        dicionario_de_as[aux_as.split()[-1]].append(aux_prefixo)
-        dicionario_de_as[aux_as.split()[-1]] = list(set(dicionario_de_as[aux_as.split()[-1]]))
-    else:
-        dicionario_de_as[aux_as.split()[-1]] = [aux_prefixo]
-
+    try:
+        if aux_as in dicionario_de_as.keys():
+            dicionario_de_as[aux_as.split()[-1]].append(aux_prefixo)
+            dicionario_de_as[aux_as.split()[-1]] = list(set(dicionario_de_as[aux_as.split()[-1]]))
+        else:
+            dicionario_de_as[aux_as.split()[-1]] = [aux_prefixo]
+    except:
+        pass
 #Abertura do arquivo
-with open('/home/marcone/Workspace/Redes/REDES/Análises/arquivo_ips.txt','r') as ponteiro:
+with open((path, 'Análises', 'arquivo_ips.txt'),'r') as ponteiro:
     while True:
         try:
             lote = ponteiro.readline()
